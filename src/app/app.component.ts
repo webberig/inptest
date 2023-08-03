9d14e57873a757c4102370fb286c47d55bbe6eed
Mathieu Maes <mathieu@zimmo.be>
2023-08-03 09:08:13 +0200

    Add progress bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {NavigationEnd, NavigationStart, Router} from "@angular/router";
+import {filter, map, merge} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'inptest';
+export class AppComponent implements OnInit {
+  loading$ = this.router.events.pipe(
+    filter(event => event instanceof NavigationStart || event instanceof NavigationEnd),
+    map(event => event instanceof NavigationStart),
+  );
+
+  constructor (
+    private router: Router,
+  ) {
+  }
+
+  ngOnInit(): void {
+  }
 }
